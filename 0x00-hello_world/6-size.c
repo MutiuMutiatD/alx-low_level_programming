539cb3d36140cb9f4b435e7fff154935f05e51b7
MutiuMutiatD <mutiumutiat@gmail.com>
2022-09-08 19:54:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,4 +10,5 @@ printf("Size of an int: %d%s\n", sizeof(int), " byte(s)");
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d%s\n", sizeof(float), " byte(s)");
-return (0);}
+return (0);
+}
