15df4c0c1fc35f1bf790d1355d15983a040baf88
MutiuMutiatD <mutiumutiat@gmail.com>
2022-09-13 18:52:14 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
-#include "_putchar"
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+ * main - Entry point
+ *
+ * Return: 0 Alwarys (Success)
+ */
+int main(void) 
 {
-  char *sh = "_putchar";
-  while (*sh)
-    {
-      _putchar(*sh); 
-      sh++;
-    }
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
   _putchar('\n');
-  return (0);
+  return (0);  
 }
