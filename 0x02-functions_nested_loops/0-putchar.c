21786e319e7351fd809f27489841fc74b046794e
MutiuMutiatD <mutiumutiat@gmail.com>
2022-09-14 17:54:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,17 @@
  * main - Entry point
  *
  * Return: 0 Alwarys (Success)
- */;
+ */
 int main(void) 
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return (0);  
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);  
 }
